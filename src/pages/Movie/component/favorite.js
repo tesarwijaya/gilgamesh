71d4ea638c8f73118e38246d9813c770b87f9888
Tesar Wijaya <tesarwijaya@192.168.0.104>
2020-07-18 21:37:35 +0700

    Add favorite null helper text

--- src/pages/Movie/component/favorite.js
+++ src/pages/Movie/component/favorite.js
@@ -3,31 +3,40 @@ import {Button,Table} from 'react-bootstrap'
 
 import isFavorite from '../../../libs/isFavorite'
 
-function Favorite({detailModalHandler,favorites,favoriteAddHandler,favoriteRemoveHandler,search}) {
+function Favorite({detailModalHandler,favorites,favoriteAddHandler,favoriteRemoveHandler}) {
   return (
-    <Table responsive>
+    <Table responsive style={{marginTop: 32, marginBottom: 32}}>
       <thead>
         <tr>
           <th>Title</th>
           <th>Year</th>
           <th>Language</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        {favorites.map(v => (
-          <tr key={v.imdbID}>
-            <td><Button onClick={() => detailModalHandler(v.imdbID)} variant="link">{v.Title}</Button></td>
-            <td>{v.Year}</td>
-            <td>{v.Language}</td>
-            <td>
-              {isFavorite(favorites, v.imdbID) ? (
-                <Button variant="outline-danger" onClick={favoriteRemoveHandler(v)}>Remove from My Favorite</Button>
-              ) : (
-                <Button variant="outline-primary" onClick={favoriteAddHandler(v)}>Add to My Favorite</Button>
-              )}
+        {favorites.length > 0 ? (
+          favorites.map(v => (
+            <tr key={v.imdbID}>
+              <td><Button onClick={() => detailModalHandler(v.imdbID)} variant="link">{v.Title}</Button></td>
+              <td>{v.Year}</td>
+              <td>{v.Language}</td>
+              <td>
+                {isFavorite(favorites, v.imdbID) ? (
+                  <Button variant="outline-danger" onClick={favoriteRemoveHandler(v)}>Remove from My Favorite</Button>
+                ) : (
+                  <Button variant="outline-primary" onClick={favoriteAddHandler(v)}>Add to My Favorite</Button>
+                )}
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan="4" style={{textAlign: "center"}}>
+              You dont have Favorite Movie
             </td>
           </tr>
-        ))}
+        )}
       </tbody>
     </Table>
   )
