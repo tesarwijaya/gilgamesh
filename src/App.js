84c145229527e589147d8addeebd339a2f1c6e90
Tesar Wijaya <tesarwijaya@192.168.0.104>
2020-07-18 11:39:59 +0700

    Add search movie dummy page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Col, Row} from 'react-bootstrap'
+
+import Movie from './pages/Movie'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col>
+          <Movie />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
